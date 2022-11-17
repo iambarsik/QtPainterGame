c178bbca39afa505dd074969bb58916c14e04171
iambarsik <gunstaria@mail.ru>
2022-11-17 15:28:25 +0300

    Main
    Main commit with basic features

---
+++ ongame.cpp
@@ -0,0 +1,13 @@
+#include "widget.h"
+
+void Widget::OnGame()   {
+
+    OnTouch();
+        // main app logic in timer period 15ms (edit in defines.h)
+    ExampleLogo.Update();
+
+
+
+
+    repaint();
+}
